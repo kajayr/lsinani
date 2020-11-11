a548be8a3de62762af9853a92a2df01b4f689e5b
Kajayr <kajayr_sinanian@yahoo.com>
2020-11-11 01:22:35 -0800

    updating contact info

--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -13,17 +13,17 @@ const Connect = (props) => {
         icon={faWindowClose}
       />
       <h1 className="connectHeader">Contact Me</h1>
-      <p className="connectText">contact@lsinani.com</p>
+      <p className="connectText">kajayr.sinanian@gmail.com</p>
       <span className="linkedIn">
         <a
           href="https://www.linkedin.com/in/leo-sinani-7b753773/"
-          target="_blank"
+          target="_blank" rel="noopener noreferrer"
         >
           <img src={linkedin} alt="LinkedIn"></img>
         </a>
       </span>
       <span className="gitHub">
-        <a href="https://github.com/" target="_blank">
+        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
           <img src={gitHub} alt="GitHub"></img>
         </a>
       </span>
